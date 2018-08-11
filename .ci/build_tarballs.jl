598811ce2657b43384ea6251436d6fd95f94a50a
Jan Strube <jan.strube@gmail.com>
2018-08-11 15:25:18 -0700

    updates for BinaryBuilder

--- .ci/build_tarballs.jl
+++ .ci/build_tarballs.jl
@@ -6,28 +6,26 @@ using BinaryBuilder
 sources = [
     "https://github.com/jstrube/LCIOWrapBuilder.git" =>
     "451f42e9b3daf41156db502ee45ee8884b77efd2",
-
-    "https://julialang-s3.julialang.org/bin/linux/x64/1.0/julia-1.0.0-linux-x86_64.tar.gz" =>
-    "bea4570d7358016d8ed29d2c15787dbefaea3e746c570763e7ad6040f17831f3",
-
 ]
 
 # Bash recipe for building across all platforms
-script = raw"""
-cd $WORKSPACE/srcdir
-tar xzf LCIOBuilder.v2.12.1.x86_64-linux-gnu.tar.gz
-tar xzf libcxxwrap-julia-0.7.v0.3.1.x86_64-linux-gnu.tar.gz
-mkdir build
-cd build
-cmake -DCMAKE_INSTALL_PREFIX=$prefix -DCMAKE_TOOLCHAIN_FILE=/opt/$target/$target.toolchain -DJulia_ROOT=/workspace/srcdir/julia-1.0.0/ ../LCIOWrapBuilder/
-make && make install
-
-"""
+function getscript(version)
+	shortversion = version[1:3]
+	return """
+	Julia_ROOT=/usr/local
+	cd /usr/local
+	curl -L "https://github.com/JuliaPackaging/JuliaBuilder/releases/download/$version/julia-$version-\$target.tar.gz" | tar -zx --strip-components=1
+	cd \$WORKSPACE/srcdir
+	mkdir build && cd build
+	cmake -DCMAKE_INSTALL_PREFIX=\$prefix -DCMAKE_TOOLCHAIN_FILE=/opt/\$target/\$target.toolchain -DJulia_ROOT=\$Julia_ROOT ../LCIOWrapBuilder/
+	VERBOSE=ON cmake --build . --config Release --target install
+	"""
+end
 
 # These are the platforms we will build for by default, unless further
 # platforms are passed in on the command line
 platforms = [
-    Linux(:x86_64, :glibc)
+    Linux(:x86_64)
 ]
 
 # The products that we will ensure are always built
@@ -37,10 +35,14 @@ products(prefix) = [
 
 # Dependencies that must be installed before this package can be built
 dependencies = [
-    "https://github.com/JuliaInterop/libcxxwrap-julia/releases/download/v0.3.1/build_libcxxwrap-julia-0.7.v0.3.1.jl",
+    "https://github.com/JuliaInterop/libcxxwrap-julia/releases/download/v0.3.2/build_libcxxwrap-julia-1.0.v0.3.2.jl"
     "https://github.com/jstrube/LCIOBuilder/releases/download/v2.00.01/build_LCIOBuilder.v2.12.1.jl"
 ]
 
 # Build the tarballs, and possibly a `build.jl` as well.
-build_tarballs(ARGS, "LCIOWrapBuilder", sources, script, platforms, products, dependencies)
+version_number = get(ENV, "TRAVIS_TAG", "")
+if version_number == ""
+    version_number = "v0.99"
+end
+build_tarballs(ARGS, "LCIOWrapBuilder", VersionNumber(version_number), sources, getscript("1.0.0"), platforms, products, dependencies)
 
