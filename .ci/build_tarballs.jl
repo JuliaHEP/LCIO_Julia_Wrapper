88c632fb75260f77a9ae848e88b25df47b078d7e
Jan Strube <jan.strube@desy.de>
2018-10-25 00:10:53 +0200

    CMake update

--- .ci/build_tarballs.jl
+++ .ci/build_tarballs.jl
@@ -14,7 +14,7 @@ function getscript(version)
 	return """
 	cd \$WORKSPACE/srcdir
 	mkdir build && cd build
-	cmake -DCMAKE_INSTALL_PREFIX=\$prefix -DCMAKE_TOOLCHAIN_FILE=/opt/\$target/\$target.toolchain -DJulia_PREFIX=\$prefix ../LCIOWrapBuilder/
+	cmake -DCMAKE_INSTALL_PREFIX=\$prefix -DCMAKE_TOOLCHAIN_FILE=/opt/\$target/\$target.toolchain -DCMAKE_FIND_ROOT_PATH=\$prefix -DJulia_PREFIX=\$prefix ../LCIOWrapBuilder/
 	VERBOSE=ON cmake --build . --config Release --target install
 	"""
 end
