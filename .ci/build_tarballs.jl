e7c65ce8f2b446d51abc410d0ab868dab788dcfe
Jan Strube <jan.strube@gmail.com>
2018-10-28 13:20:35 -0700

    updates to build dirs

--- .ci/build_tarballs.jl
+++ .ci/build_tarballs.jl
@@ -4,8 +4,7 @@ using BinaryBuilder
 
 # Collection of sources required to build LCIOWrapBuilder
 sources = [
-#    "https://github.com/jstrube/LCIOWrapBuilder.git" => "451f42e9b3daf41156db502ee45ee8884b77efd2",
-    "jstrube/LCIOWrapBuilder/"
+    "LCIOWrapBuilder"
 ]
 
 # Bash recipe for building across all platforms
@@ -14,7 +13,7 @@ function getscript(version)
 	return """
 	cd \$WORKSPACE/srcdir
 	mkdir build && cd build
-	cmake -DCMAKE_INSTALL_PREFIX=\$prefix -DCMAKE_TOOLCHAIN_FILE=/opt/\$target/\$target.toolchain -DCMAKE_FIND_ROOT_PATH=\$prefix -DJulia_PREFIX=\$prefix ../LCIOWrapBuilder
+	cmake -DCMAKE_INSTALL_PREFIX=\$prefix -DCMAKE_TOOLCHAIN_FILE=/opt/\$target/\$target.toolchain -DCMAKE_FIND_ROOT_PATH=\$prefix -DJulia_PREFIX=\$prefix ..
 	VERBOSE=ON cmake --build . --config Release --target install
 	"""
 end
