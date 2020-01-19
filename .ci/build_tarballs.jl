6f18a0166181f3a790ae5103072acba59e988fd6
Jan Strube <jan.strube@gmail.com>
2020-01-19 15:25:39 -0800

    updating build script

--- .ci/build_tarballs.jl
+++ .ci/build_tarballs.jl
@@ -7,42 +7,40 @@ sources = [
     "LCIOWrapBuilder"
 ]
 
+name = "LCIOWrapBuilder"
+version = VersionNumber(get(ENV, "TRAVIS_TAG", "0.99"))
+
 # Bash recipe for building across all platforms
-function getscript(version)
-	shortversion = version[1:3]
-	return """
-	cd \$WORKSPACE/srcdir
+script = raw"""
+	wget https://github.com/Gnimuc/JuliaBuilder/releases/download/v1.3.0/julia-1.3.0-${target}.tar.gz
+	tar xzf julia-1.3.0-${target}.tar.gz
+	export PATH=$(pwd)/juliabin/bin:${PATH}
+	ln -s ${WORKSPACE}/srcdir/juliabin/include/ /opt/${target}/${target}/sys-root/usr/local
+	cd ${WORKSPACE}/srcdir
 	mkdir build && cd build
-	cmake -DCMAKE_INSTALL_PREFIX=\$prefix -DCMAKE_TOOLCHAIN_FILE=/opt/\$target/\$target.toolchain -DCMAKE_FIND_ROOT_PATH=\$prefix -DJulia_PREFIX=\$prefix ..
+	cmake -DCMAKE_INSTALL_PREFIX=$prefix -DCMAKE_TOOLCHAIN_FILE=${CMAKE_TARGET_TOOLCHAIN} -DCMAKE_BUILD_TYPE=Release ..
+	#cmake -DCMAKE_INSTALL_PREFIX=${prefix} -DCMAKE_TOOLCHAIN_FILE=/opt/${target}/${target}.toolchain -DCMAKE_FIND_ROOT_PATH=${prefix} -DJulia_PREFIX=${prefix} ..
 	VERBOSE=ON cmake --build . --config Release --target install
-	"""
-end
+"""
 
 # These are the platforms we will build for by default, unless further
 # platforms are passed in on the command line
 platforms = Platform[
-    Linux(:x86_64, libc=:glibc, compiler_abi=CompilerABI(:gcc7, :cxx11)),
-    Linux(:x86_64, libc=:glibc, compiler_abi=CompilerABI(:gcc8, :cxx11)),
-    MacOS(:x86_64, compiler_abi=CompilerABI(:gcc7)),
-    MacOS(:x86_64, compiler_abi=CompilerABI(:gcc8)),
+    Linux(:x86_64, libc=:glibc),
+    MacOS(:x86_64),
 ]
 
 # The products that we will ensure are always built
-products(prefix) = [
-    LibraryProduct(prefix, "liblciowrap", :lciowrap)
+products = [
+    LibraryProduct("liblciowrap", :lciowrap)
 ]
 
 # Dependencies that must be installed before this package can be built
 dependencies = [
-	"https://github.com/JuliaInterop/libcxxwrap-julia/releases/download/v0.6.2/build_libcxxwrap-julia-1.0.v0.6.2.jl"
-	"https://github.com/JuliaPackaging/JuliaBuilder/releases/download/v1.0.0-2/build_Julia.v1.0.0.jl"
-	"https://github.com/jstrube/LCIOBuilder/releases/download/v2.12.1-4/build_LCIOBuilder.v2.12.1-4.jl"
+	"libcxxwrap_julia_jll",
+	"LCIO_jll"
 ]
 
 # Build the tarballs, and possibly a `build.jl` as well.
-version_number = get(ENV, "TRAVIS_TAG", "")
-if version_number == ""
-    version_number = "v0.99"
-end
-build_tarballs(ARGS, "LCIOWrapBuilder-1.0", VersionNumber(version_number), sources, getscript("1.0.0"), platforms, products, dependencies)
+build_tarballs(ARGS, name, version, sources, script, platforms, products, dependencies; preferred_gcc_version=v"8")
 
